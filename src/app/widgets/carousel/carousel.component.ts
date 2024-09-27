d6059d73e98ae4a6d72af569453adfd02c5d1b2d
OmelchenkoIgor <omelchenkoigor7777@gmail.com>
2024-09-28 01:22:51 +0300

    update: carousel

--- src/app/widgets/carousel/carousel.component.ts
+++ src/app/widgets/carousel/carousel.component.ts
@@ -1,24 +1,64 @@
+import {Component, computed, inject, Signal, ViewChild, ElementRef, AfterViewInit, HostListener} from '@angular/core';
 import {GetTrendingCinemaCommand} from '@commands/get-trending-cinema.command';
 import {TrendingCinemaRepository} from '@data/repositories';
 import {CarouselCardComponent} from '@shared/components';
-import {Component, computed, inject, Signal} from '@angular/core';
-import {NgClass} from '@angular/common';
 import {previewCinema} from '@data/entities';
+import {NgClass} from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.scss',
-  imports: [CarouselCardComponent, NgClass]
+  styleUrls: ['./carousel.component.scss'],
+  imports: [CarouselCardComponent, NgClass],
 })
-export class CarouselComponent {
+export class CarouselComponent implements AfterViewInit {
   public readonly trendingCinema: TrendingCinemaRepository = inject(TrendingCinemaRepository);
   private readonly getTrendingCinema: GetTrendingCinemaCommand = inject(GetTrendingCinemaCommand);
 
   public readonly trending: Signal<Array<previewCinema>> = computed(() => this.trendingCinema.isTrendingCinemaList());
 
+  @ViewChild('carouselContainer') carouselContainer!: ElementRef;
+  public canScrollLeft: boolean = false;
+  public canScrollRight: boolean = false;
+  public showScrollButtons: boolean = false;
+
   constructor() {
     this.getTrendingCinema.execute();
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.checkScrollPosition();
+      this.updateShowScrollButtons();
+    }, 100);
+  }
+
+  public scrollLeft(): void {
+    const container = this.carouselContainer.nativeElement;
+    const cardWidth = container.querySelector('app-carousel-card')?.offsetWidth || 0;
+    container.scrollBy({left: -3 * cardWidth, behavior: 'smooth'});
+  }
+
+  public scrollRight(): void {
+    const container = this.carouselContainer.nativeElement;
+    const cardWidth = container.querySelector('app-carousel-card')?.offsetWidth || 0;
+    container.scrollBy({left: 3 * cardWidth, behavior: 'smooth'});
+  }
+
+  public checkScrollPosition(): void {
+    const container = this.carouselContainer.nativeElement;
+    this.canScrollLeft = container.scrollLeft > 0;
+    this.canScrollRight = container.scrollWidth > container.scrollLeft + container.clientWidth;
+  }
+
+  @HostListener('window:resize')
+  onResize(): void {
+    this.checkScrollPosition();
+    this.updateShowScrollButtons();
+  }
+
+  private updateShowScrollButtons(): void {
+    this.showScrollButtons = window.innerWidth > 768;
   }
 }
