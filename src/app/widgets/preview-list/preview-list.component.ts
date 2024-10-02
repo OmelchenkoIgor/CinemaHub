3f33e988775b577e2caaf57347a03961f27c14c5
OmelchenkoIgor <omelchenkoigor7777@gmail.com>
2024-10-03 00:01:27 +0300

    fix: content upload

--- src/app/widgets/preview-list/preview-list.component.ts
+++ src/app/widgets/preview-list/preview-list.component.ts
@@ -4,8 +4,8 @@ import {PreviewCardComponent} from '@features/preview-card';
 import {PreviewCinemaRepository} from '@data/repositories';
 import {LoadingSpinnerComponent} from '@shared/components';
 import {previewCinema} from '@data/entities';
+import {Category, Type} from '@shared/type';
 import {TitlePipe} from '@shared/pipe';
-import {Category} from '@shared/type';
 
 @Component({
   standalone: true,
@@ -16,35 +16,54 @@ import {Category} from '@shared/type';
 })
 export class PreviewListComponent implements OnDestroy {
   public readonly category: InputSignal<Category> = input.required();
-
   private readonly getCinema: GetCinemaCommand = inject(GetCinemaCommand);
-
   public readonly previewCinema: PreviewCinemaRepository = inject(PreviewCinemaRepository);
+
+  private readonly categoryActions: { [key: string]: { action: Type; type: Category } } = {
+    'all': {action: 'trending', type: 'all'},
+    'movie': {action: 'discover', type: 'movie'},
+    'tv': {action: 'discover', type: 'tv'}
+  };
 
   protected isLoading: boolean = true;
   protected readonly previewList: Signal<Array<previewCinema>> = computed(() => this.previewCinema.isPreviewCinemaList());
 
   constructor() {
     effect(() => {
-      if (this.category() === 'all') {
-        this.getCinema.execute('trending', 'all', 1, 'en');
-      } else if (this.category() === 'movie') {
-        this.getCinema.execute('discover', 'movie', 1, 'en');
-      } else if (this.category() === 'tv') {
-        this.getCinema.execute('discover', 'tv', 1, 'en');
-      }
+      this.fetchData(1);
     });
 
     effect(() => {
       if (this.previewList() && this.previewList().length) {
         this.isLoading = false;
+        setTimeout(() => this.checkContentHeight());
       }
     });
   }
 
-  ngOnDestroy(): void {
-    this.previewCinema.setNewPage(2);
-    this.previewCinema.setSearchQuery('');
+  private fetchData(page: number): void {
+    const categoryInfo: { action: Type; type: Category } = this.categoryActions[this.category()];
+
+    if (categoryInfo) {
+      this.getCinema.execute(categoryInfo.action, categoryInfo.type, page, 'en');
+    }
+  }
+
+  private loadMoreData(): void {
+    this.isLoading = true;
+    const page: number = this.previewCinema.isPage();
+
+    this.fetchData(page);
+    this.previewCinema.setNewPage(page + 1);
+  }
+
+  private checkContentHeight(): void {
+    const contentHeight: number = document.body.scrollHeight;
+    const viewportHeight: number = window.innerHeight;
+
+    if (contentHeight <= viewportHeight && !this.isLoading && !this.previewCinema.isSearchQuery().length) {
+      this.loadMoreData();
+    }
   }
 
   @HostListener('window:scroll', [])
@@ -53,17 +72,12 @@ export class PreviewListComponent implements OnDestroy {
     const threshold: number = document.body.offsetHeight * 0.9;
 
     if (scrollPosition >= threshold && !this.isLoading && !this.previewCinema.isSearchQuery().length) {
-      this.isLoading = true;
-      if (this.category() === 'all') {
-        this.getCinema.execute('trending', 'all', this.previewCinema.isPage(), 'en');
-        this.previewCinema.setNewPage(this.previewCinema.isPage() + 1);
-      } else if (this.category() === 'movie') {
-        this.getCinema.execute('discover', 'movie', this.previewCinema.isPage(), 'en');
-        this.previewCinema.setNewPage(this.previewCinema.isPage() + 1);
-      } else if (this.category() === 'tv') {
-        this.getCinema.execute('discover', 'tv', this.previewCinema.isPage(), 'en');
-        this.previewCinema.setNewPage(this.previewCinema.isPage() + 1);
-      }
+      this.loadMoreData();
     }
+  }
+
+  ngOnDestroy(): void {
+    this.previewCinema.setNewPage(2);
+    this.previewCinema.setSearchQuery('');
   }
 }
